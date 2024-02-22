fefb45256c52f7a6369dd82c23507e3f86f4da9d
Jack Heuberger <jackheuberger5@gmail.com>
2024-02-22 12:15:41 -0600

    create Dockerfile & deployment action

--- api/src/clients/Seats.Aero/client.ts
+++ api/src/clients/Seats.Aero/client.ts
@@ -3,32 +3,39 @@ import { ResponseData, SeatsCachedSearchParams } from "./types";
 const sdk = require('api')('@seatsaero/v1.0#cqdn9uslsnn1wyf');
 
 class SeatsAero implements SearchClientInterface {
-    constructor(api_key: string) {
-        sdk.auth(api_key)
-    }
+  api_key: string;
+  constructor(api_key: string) {
+    this.api_key = api_key
+  }
 
-    find_route(params: SeatsCachedSearchParams): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-
-            sdk.cachedSearch(params)
-                .then(({ data }: { data: any }) => {
-                    const parsedResponse: ResponseData = {
-                        data: data.data.map((item: any) => ({
-                          ...item,
-                          Route: {
-                            ...item.Route,
-                          },
-                        })),
-                        count: data.count,
-                        hasMore: data.hasMore,
-                        cursor: data.cursor
-                      } as ResponseData;
-                      console.log(parsedResponse)
-                    resolve(true)
-                })
-                .catch(console.error)
+  find_route(params: SeatsCachedSearchParams): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      sdk.auth(this.api_key)
+      sdk.cachedSearch({
+        origin_airport: "JFK",
+        destination_airport: "LAX",
+        cabin: "economy",
+        start_date: "2024-05-01",
+        end_date: "2024-05-01" // TODO: as of now, the end_date is not optional
+    })
+        .then(({ data }: { data: any }) => {
+          const parsedResponse: ResponseData = {
+            data: data.data.map((item: any) => ({
+              ...item,
+              Route: {
+                ...item.Route,
+              },
+            })),
+            count: data.count,
+            hasMore: data.hasMore,
+            cursor: data.cursor
+          } as ResponseData;
+          console.log(parsedResponse)
+          resolve(true)
         })
-    }
+        .catch(console.error)
+    })
+  }
 }
 
 export { SeatsAero }
