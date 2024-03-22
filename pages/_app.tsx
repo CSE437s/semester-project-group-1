93fb0bb6012d04ec9367297d5f90d9527c325383
Jack Heuberger <jackheuberger5@gmail.com>
2024-03-22 11:35:48 -0500

    Put navbar on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,15 @@ import "/styles/globals.css";
 import { type Session, createPagesBrowserClient } from "@supabase/auth-helpers-nextjs"
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { type AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
   const [supabaseClient] = useState(() => createPagesBrowserClient())
   return (
-    <SessionContextProvider 
+    <SessionContextProvider
       supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
+      initialSession={pageProps.initialSession}>
+      <Navbar />
       <Component {...pageProps} />
     </SessionContextProvider>
   )
